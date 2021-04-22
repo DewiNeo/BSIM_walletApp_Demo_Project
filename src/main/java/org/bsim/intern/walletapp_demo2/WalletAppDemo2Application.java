e0c99ccf24e7dd61b01f363eb1dcabc14b56092b
meimei <dewi.ismaneti@binus.ac.id>
2021-04-22 21:15:13 +0700

    inisialisasi_walletApp_demo2

---
+++ src/main/java/org/bsim/intern/walletapp_demo2/WalletAppDemo2Application.java
@@ -0,0 +1,13 @@
+package org.bsim.intern.walletapp_demo2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalletAppDemo2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WalletAppDemo2Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/bsim/intern/walletapp_demo2/WalletAppDemo2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.bsim.intern.walletapp_demo2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalletAppDemo2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
